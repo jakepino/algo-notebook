4b08c756c3bde749b0f7fe19e744e764b0b284d8
jakepino <jakepino@yahoo.com>
2020-10-21 17:06:24 -0400

    added oddNumbers

---
+++ binary-search/OddNumberOfDigits.js
@@ -0,0 +1,13 @@
+// Given a list of positive integers nums,
+// return the number of integers that have odd number of digits.
+
+// Example 1
+// Input
+
+// nums = [1, 800, 2, 10, 3]
+// Output
+
+// 4
+// Explanation
+
+// [1, 800, 2, 3] have odd number of digits.
