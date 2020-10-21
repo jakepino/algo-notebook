060e3ad1696ca3ddb6c3e9c801b38f5500d7f032
jakepino <jakepino@yahoo.com>
2020-10-21 17:09:00 -0400

    odds solution

--- binary-search/OddNumberOfDigits.js
+++ binary-search/OddNumberOfDigits.js
@@ -11,3 +11,18 @@
 // Explanation
 
 // [1, 800, 2, 3] have odd number of digits.
+
+//solution
+function solve(nums) {
+	let counter = 0;
+	for (let i = 0; i < nums.length; i++) {
+		let val = nums[i].toString();
+		if (parseInt(val.length) % 2 !== 0) {
+			counter++;
+		}
+	}
+	return counter;
+}
+let nums = [1, 800, 2, 10, 3];
+console.log(solve(nums));
+//except 4
