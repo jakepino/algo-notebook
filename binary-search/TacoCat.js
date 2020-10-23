d7a097a3408ee0a48fcbb9eba2a16624400c4bd7
jakepino <jakepino@yahoo.com>
2020-10-23 16:09:45 -0400

    tacocat add

---
+++ binary-search/TacoCat.js
@@ -0,0 +1,26 @@
+// Given a string s, return whether it is a palindrome.
+// A palindrome is when the word is the same forwards and backwards.
+
+// For example, "tacocat" is a palindrome.
+
+// Example 1
+// Input
+
+// s = "racecar"
+// Output
+
+// true
+// Example 2
+// Input
+
+// s = "evilolive"
+// Output
+
+// true
+// Example 3
+// Input
+
+// s = "palindrome"
+// Output
+
+// false
