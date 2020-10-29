2e5af1c3aacfea0c509e5e2910b8773667354b46
jakepino <jakepino@yahoo.com>
2020-10-28 22:52:30 -0400

    solution oddly

--- binary-search/OddlySpecific.js
+++ binary-search/OddlySpecific.js
@@ -13,3 +13,11 @@
 // Explanation
 
 // The first 5 odd integers are [1, 3, 5, 7, 9] and its sum is 25.
+
+// solution
+function solve(n) {
+	return n ** 2;
+}
+
+console.log(solve(5));
+//returns 25
