8bbf5a21d302bd543beb1e39930042f5e737a394
jakepino <jakepino@yahoo.com>
2020-11-06 18:25:01 -0500

    missed save

---
+++ binary-search/PigeonHole.js
@@ -0,0 +1,24 @@
+// You are given a list nums of length n + 1 picked from the range 1, 2, ..., n.
+// By the pigeonhole principle, there must be a duplicate.
+// Find and return it. There is guaranteed to be exactly one duplicate.
+
+// Bonus: Can you do this in linear time and constant space?
+
+// Constraints
+
+// n ≤ 100,000
+// Example 1
+// Input
+
+// nums = [1, 2, 3, 1]
+// Output
+
+// 1
+// Example 2
+// Input
+
+// nums = [4, 2, 1, 3, 2]
+// Output
+
+// 2
+//
