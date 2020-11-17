058a424c801074fbf101a37f54ff264359f9a8a8
jakepino <jakepino@yahoo.com>
2020-11-17 03:53:05 -0500

    added primes question

---
+++ binary-search/GeneratePrimes.js
@@ -0,0 +1,27 @@
+// Given a number n, return a list of all prime numbers smaller than or equal to n in ascending order.
+
+// For example, given the number 10, return the list [2, 3, 5, 7].
+
+// Note: 1 is not a prime number.
+
+// Example 1
+// Input
+
+// n = 3
+// Output
+
+// [2, 3]
+// Example 2
+// Input
+
+// n = 10
+// Output
+
+// [2, 3, 5, 7]
+// Example 3
+// Input
+
+// n = 20
+// Output
+
+// [2, 3, 5, 7, 11, 13, 17, 19]
