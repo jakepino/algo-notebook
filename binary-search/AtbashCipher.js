e5ee0751b56a17c352f171e887d8f0fed15c3d9b
jakepino <jakepino@yahoo.com>
2020-11-23 15:09:56 -0500

    better atbash

--- binary-search/AtbashCipher.js
+++ binary-search/AtbashCipher.js
@@ -22,3 +22,40 @@
     //         return newArr.join("")
     //     }
     
+
+    // solution 2
+function solve(text) {
+    const cypher = {
+        'a': 'z',
+        'b': 'y',
+        'c': 'x',
+        'd': 'w',
+        'e': 'v',
+        'f': 'u',
+        'g': 't',
+        'h': 's',
+        'i': 'r',
+        'j': 'q',
+        'k': 'p',
+        'l': 'o',
+        'm': 'n',
+        'n': 'm',
+        'o': 'l',
+        'p': 'k',
+        'q': 'j',
+        'r': 'i',
+        's': 'h',
+        't': 'g',
+        'u': 'f',
+        'v': 'e',
+        'w': 'd',
+        'x': 'c',
+        'y': 'b',
+        'z': 'a',
+    }
+
+    return text.split('').map(char => {
+        return cypher[char]
+    }).join('')
+
+}
