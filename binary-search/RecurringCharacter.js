07d9bef05d2263bb380e4f75a6a649d51e47d972
jakepino <jakepino@yahoo.com>
2020-12-01 12:27:14 -0500

    recurring char

---
+++ binary-search/RecurringCharacter.js
@@ -0,0 +1,26 @@
+// Given a string, return the index of the first recurring character in it. 
+// If there are no recurring characters, return -1.
+
+// Example 1
+// Input
+// s = "abcda"
+// Output
+// 4
+// Explanation
+// The a at index 4 is the first recurring character.
+
+// Example 2
+// Input
+// s = "abcde"
+// Output
+// -1
+// Explanation
+// No recurring characters in this string.
+
+// Example 3
+// Input
+// s = "aaaaa"
+// Output
+// 1
+// Explanation
+// We want the first recurring character.
