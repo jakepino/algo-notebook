22d85549278ffb4d2f8a1afa2d69c5cf486290f1
jakepino <jakepino@yahoo.com>
2020-12-25 22:49:07 -0500

    pangram add

---
+++ binary-search/easy-questions/Pangram.js
@@ -0,0 +1,14 @@
+// Given a string s, representing a sentence, return whether every letter (case-insensitive) of the alphabet is used at least once.
+
+// Example 1
+// Input
+// s = "A quick brown fox jumps over the lazy dog"
+// Output
+// true
+// Example 2
+// Input
+// s = "The jay, pig, fox, tiger and my wolves quack!"
+// Output
+// false
+// Explanation
+// This sentence is missing a "z"
