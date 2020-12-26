0f7df289dae0a41acc7660db1a43df6be3f4339c
jakepino <jakepino@yahoo.com>
2020-12-25 22:47:11 -0500

    hamming distance

---
+++ binary-search/easy-questions/HammingDistance.js
@@ -0,0 +1,13 @@
+// Given two integers x, and y return the number of positions where their values differ in their binary representations as a 32-bit integer.
+
+// Constraints
+
+// 0 ≤ x, y < 2 ** 31 
+// Example 1
+// Input
+// x = 9
+// y = 5
+// Output
+// 2
+// Explanation
+// 9 in binary is 1001 and 5 in binary is 0101, so indices 2 and 3 are different.
