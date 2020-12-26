14ae65f5d62d8999c743b39563208fa2335188b6
jakepino <jakepino@yahoo.com>
2020-12-25 22:47:40 -0500

    hamming sol

--- binary-search/easy-questions/HammingDistance.js
+++ binary-search/easy-questions/HammingDistance.js
@@ -11,3 +11,24 @@
 // 2
 // Explanation
 // 9 in binary is 1001 and 5 in binary is 0101, so indices 2 and 3 are different.
+
+class Solution {
+    solve(x, y) {
+        let diff = 0
+        x = x.toString(2)
+        y = y.toString(2)
+
+        let largest = x.length >= y.length ? x.length : y.length
+        if (x.length < y.length) x = '0'.repeat(y.length - x.length) + x
+        if (y.length < x.length) y = '0'.repeat(x.length - y.length) + y
+
+        for (let i = 0; i < largest; i++) {
+            if (x[i] !== y[i]) {
+                diff += 1
+            }
+        }
+
+        return diff
+
+    }
+}
