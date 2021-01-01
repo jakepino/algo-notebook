5966c47eea4774a69f11b998985c18c14b2c68bc
jakepino <jakepino@yahoo.com>
2021-01-01 18:54:50 -0500

    consec dup test

---
+++ binary-search/easy-questions/ConsecutiveDuplicates.js
@@ -0,0 +1,13 @@
+// Given a string s, consisting of "X" and "Y"s, 
+// delete the minimum number of characters such that there's no consecutive "X" and no consecutive "Y".
+
+// Constraints
+
+// n ≤ 100,000 where n is the length of s
+// Example 1
+// Input
+// s = "YYYXYXX"
+// Output
+// "YXYX"
+// Explanation
+// One solution is to delete the first two "Y"s and the last "X".
