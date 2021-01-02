f7e322e8f01a7c531b0e1a4bc2bc76dfa84222ed
jakepino <jakepino@yahoo.com>
2021-01-01 19:08:24 -0500

    palidrome easy

---
+++ binary-search/easy-questions/CheckPalidrome.js
@@ -0,0 +1,29 @@
+// Given a string s, return whether it is a palindrome.
+
+// Constraints
+
+// n ≤ 100,000 where n is the length of s
+// Example 1
+// Input
+// s = "racecar"
+// Output
+// true
+// Example 2
+// Input
+// s = "evilolive"
+// Output
+// true
+// Example 3
+// Input
+// s = "palindrome"
+// Output
+// false
+function solve(s) {
+    for(let i = 0; i < s.length; i++){
+        if(s[i] !== s[s.length - i -1]){
+            console.log(s[s.length - i])
+            return false
+        }
+    }
+    return true
+}
