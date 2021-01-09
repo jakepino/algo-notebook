5f23b87799be76d5a6b1409f7a09f96d54e6b8f0
jakepino <jakepino@yahoo.com>
2021-01-08 20:47:01 -0500

    leaderboard

---
+++ binary-search/easy-questions/Leaderboard.js
@@ -0,0 +1,10 @@
+You are given a list of integers nums, representing the number of chess matches each person has won. Return a relative ranking (0-indexed) of each person. If two players have won the same amount, their ranking should be the same.
+
+Constraints
+
+n ≤ 100,000 where n is the length of nums
+Example 1
+Input
+nums = [50, 30, 50, 90, 10]
+Output
+[1, 2, 1, 0, 3]
