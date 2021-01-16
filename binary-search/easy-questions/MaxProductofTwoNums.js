107b0548ddb1c655410e4713efaa94b11f4bc4bb
jakepino <jakepino@yahoo.com>
2021-01-15 22:24:42 -0500

    max product of nums

---
+++ binary-search/easy-questions/MaxProductofTwoNums.js
@@ -0,0 +1,29 @@
+// Given a list of integers nums, find the largest product of two distinct elements.
+
+// Constraints
+
+// n ≤ 100,000 where n is the length of nums
+// Example 1
+// Input
+// nums = [5, 1, 7]
+// Output
+// 35
+// Explanation
+// 35 is the largest product that can be made from 5 * 7
+
+// Example 2
+// Input
+// nums = [7, 1, 7]
+// Output
+// 49
+// Explanation
+// 49 is the largest product that can be made from 7 * 7. The values can be the same but they must be separate elements.
+
+// Example 3
+// Input
+// nums = [-5, 1, -7]
+// Output
+// 35
+// Explanation
+// 35 is the largest product that can be made from -5 * -7.
+// solution 1
