e193ef467bb7959192fad8418dedeb5c21f71912
jakepino <jakepino@yahoo.com>
2021-02-05 22:45:38 -0500

    word bijection added to solve later

---
+++ binary-search/easy-questions/PatternToWordBijection.js
@@ -0,0 +1,14 @@
+// Given two strings s and p, return whether s follows the pattern in p. 
+// That is, return whether each character in p can map to a non-empty word such that it maps to s.
+
+// Constraints
+
+// n ≤ 100,000 where n is the length of s
+// Example 1
+// Input
+// s = "hello world world world hello"
+// p = "abbba"
+// Output
+// true
+// Explanation
+// We can map "a" = "hello" and "b" = "world"
