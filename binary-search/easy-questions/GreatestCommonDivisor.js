514c7cf57e23f001a8cfa2b7a85eba33d8ebe7a4
jakepino <jakepino@yahoo.com>
2021-02-12 21:39:41 -0500

    greatest common divisor added

---
+++ binary-search/easy-questions/GreatestCommonDivisor.js
@@ -0,0 +1,21 @@
+// Given a list of positive integers nums, 
+// return the largest positive integer that divides each of the integers.
+
+// Constraints
+
+// 1 ≤ n ≤ 100,000 where n is the length of nums
+// Example 1
+// Input
+// nums = [6, 12, 9]
+// Output
+// 3
+// Explanation
+// 3 is the largest integer that divides into 6, 12, and 9.
+
+// Example 2
+// Input
+// nums = [6, 7, 9]
+// Output
+// 1
+// Explanation
+// 7 is a prime number so only 1 divides into it.
