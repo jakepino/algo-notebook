64865ac8db27ba712fe13bd457d3b1bc1977e803
jakepino <jakepino@yahoo.com>
2021-02-12 21:40:22 -0500

    GCD solved

--- binary-search/easy-questions/GreatestCommonDivisor.js
+++ binary-search/easy-questions/GreatestCommonDivisor.js
@@ -19,3 +19,27 @@
 // 1
 // Explanation
 // 7 is a prime number so only 1 divides into it.
+
+class Solution {
+    solve(nums) {
+            let divisor = 1
+            let greatestDivisor = 1
+            let count = 1
+            let smallest = Math.min(...nums)
+            while(divisor <= smallest){
+            // Will go thorugh all the numbers checking if they are divisable by the current divisor. Increasing the count if they are.
+                for (let i = 0; i < nums.length; i++){
+                    if (nums[i] % divisor === 0){
+                        console.log(count)
+                        count++
+                            }
+                    if (count === nums.length){
+                        greatestDivisor = divisor
+                            }
+                    }
+                count = 0
+                divisor += 1
+            }
+            return greatestDivisor
+        }
+    } 
