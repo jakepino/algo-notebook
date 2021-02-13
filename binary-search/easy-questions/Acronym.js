ba26a6dc5f34053295bc9513f3fc02eb6a531727
jakepino <jakepino@yahoo.com>
2021-02-12 21:41:14 -0500

    Acroynm added

---
+++ binary-search/easy-questions/Acronym.js
@@ -0,0 +1,16 @@
+// Given a string s representing a phrase, return its acronym. 
+// Acronyms should be capitalized and should not include the word "and".
+
+// Constraints
+
+// 0 ≤ n ≤ 100,000 where n is the length of s
+// Example 1
+// Input
+// s = "For your information"
+// Output
+// "FYI"
+// Example 2
+// Input
+// s = "National Aeronautics and Space Administration"
+// Output
+// "NASA"
