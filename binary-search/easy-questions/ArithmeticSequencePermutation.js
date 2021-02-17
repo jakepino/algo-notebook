1685535f7051b75bb40b9701fefd1c9ac35f8282
jakepino <jakepino@yahoo.com>
2021-02-17 14:42:19 -0500

    arithmetic sequence

---
+++ binary-search/easy-questions/ArithmeticSequencePermutation.js
@@ -0,0 +1,41 @@
+// Given a list of integers nums, 
+// return whether you can rearrange the order of nums 
+// such that the difference between every consecutive two numbers is the same.
+
+// Constraints
+
+// n ≤ 100,000 where n is the length of nums
+// Example 1
+// Input
+// nums = [7, 1, 5, 3]
+// Output
+// true
+// Explanation
+// If we rearrange nums to [1, 3, 5, 7], 
+// then the difference between every two consecutive numbers is 2.
+
+// Example 2
+// Input
+// nums = [1, 5, 1, 5, 1, 5]
+// Output
+// false
+// Explanation
+// The difference between every consecutive two numbers alternates between 4 and -4
+class Solution {
+    solve(nums) {
+        if(nums.length === 2) {
+            return true
+        }
+        
+        let sorted = nums.sort((a, b) => a-b)
+        let diff = sorted[1] - sorted[0]
+
+        for(let i = 1; i < sorted.length; i++){
+            if(diff != (sorted[i]-sorted[i-1])){
+                return false
+            }
+        }
+        
+        return true
+    }
+}
