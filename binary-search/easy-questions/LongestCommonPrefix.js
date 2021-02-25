94e1789497cd7428e72deb92ddc6985c5dd81a66
jakepino <jakepino@yahoo.com>
2021-02-25 15:29:53 -0500

    longest common prefix

---
+++ binary-search/easy-questions/LongestCommonPrefix.js
@@ -0,0 +1,26 @@
+// Given a list of lowercase alphabet strings words, return the longest common prefix.
+
+// Example 1
+// Input
+// words = ["anthony", "ant", "antigravity"]
+// Output
+// "ant"
+// Explanation
+// "ant" is the longest common prefix between the three strings.
+class Solution {
+    solve(words) {
+        let string = ""
+        let counter = 0
+        for(let i = 0; i < words[0].length; i++){
+            let letter = words[0][i]
+            for(let j = 0; j < words.length; j++){
+                if(words[j][i] !== letter){
+                    return string
+                }
+            }
+                string += letter
+        }
+        
+        return string  
+    }
+}
