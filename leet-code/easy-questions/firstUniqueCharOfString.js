03b539ac59804472d41074265f554057091cfff5
jakepino <jakepino@yahoo.com>
2021-03-22 12:06:40 -0400

    firstUniqCharOfString optimized

--- leet-code/easy-questions/firstUniqueCharOfString.js
+++ leet-code/easy-questions/firstUniqueCharOfString.js
@@ -13,22 +13,36 @@
 // Note: You may assume the string contains only lowercase English letters.
 
 //solution 1
+// var firstUniqChar = function(s) {
+//     let hash = {}
+    
+//     for(let i = 0; i < s.length; i++){
+//         hash[s[i]] ? hash[s[i]]++ : hash[s[i]] = 1
+//     }
+    
+//     let keys = Object.keys(hash)
+//     let values = Object.values(hash)
+//     console.log(values)
+//     for(let i = 0; i < values.length; i++){
+//         if(values[i] === 1){
+//             let key = keys[i]
+//             return s.indexOf(key)
+//         }
+//     }
+    
+//     return -1
+// };
+
+//Much Faster Solution
 var firstUniqChar = function(s) {
-    let hash = {}
-    
+    if(s.length == 1) return 0;
+       
     for(let i = 0; i < s.length; i++){
-        hash[s[i]] ? hash[s[i]]++ : hash[s[i]] = 1
+        if((s.lastIndexOf(s.charAt(i)) == i) &&
+            (s.indexOf(s.charAt(i)) == i))
+           {
+                return i;
+           }
     }
-    
-    let keys = Object.keys(hash)
-    let values = Object.values(hash)
-    console.log(values)
-    for(let i = 0; i < values.length; i++){
-        if(values[i] === 1){
-            let key = keys[i]
-            return s.indexOf(key)
-        }
-    }
-    
-    return -1
+    return -1;
 };
